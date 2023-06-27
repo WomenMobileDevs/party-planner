d7698cc5fe1d4fe7df93b69902850399d11b309f
Lakshay saini <lakshay.saini@finmo.net>
2023-06-27 21:53:27 +0530

    feat: added ui for event organizer form

--- data.js
+++ data.js
@@ -2,3 +2,13 @@ export const cities = [
   {label: 'Jaipur', value: 'Jaipur'},
   {label: 'Kolkata', value: 'Kolkata'},
 ];
+
+export const gender_list = [
+  {label: 'Women', value: 'Women'},
+  {label: 'Men', value: 'Men'},
+];
+
+export const event_list = [
+  {label: 'Birthday', value: 'Birthday'},
+  {label: 'Marriage', value: 'Marriage'},
+];
