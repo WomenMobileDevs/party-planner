4f7c25a3c3dcf641e70b433bcd57f59a712c8faa
shreya-mishra <shreyamishra040398@gmail.com>
2023-10-01 11:13:48 +0530

    added some changes to create staging

--- App.tsx
+++ App.tsx
@@ -1,18 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, {useEffect, type PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
